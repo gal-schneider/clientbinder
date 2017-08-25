98cf68fbba8b7d37a97d82cce6ecef118872b768
roni <roni@10.0.0.15>
2017-08-25 20:41:15 +0300

    fix package name in classes

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,4 +1,4 @@
-package com.example;
+package main.java.com.example;
 
 import java.io.IOException;
 import java.util.logging.Logger;
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,4 +1,4 @@
-package com.example;
+package main.java.com.example;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;

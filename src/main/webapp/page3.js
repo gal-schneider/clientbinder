d96fe4679dbddb8ebeb936ed113f223f4e18ea1e
gal-schneider <gal.schneider@gmail.com>
2018-11-08 23:48:07 +0200

    Update page3.js

--- src/main/webapp/page3.js
+++ src/main/webapp/page3.js
@@ -1,2 +1,2 @@
-var txt = document.createTextNode(" This text was added to the DIV.");
+var txt = document.createTextNode(" This text was added to the DIV. 777");
 document.getElementById("1").appendChild(txt);
